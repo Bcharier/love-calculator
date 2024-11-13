3b497df8129626698b23782f350ae154ec0cf8d7
bcharier <baptiste.charier2022@campus-eni.fr>
2024-11-13 15:53:44 +0100

    add canMatch guard + remove spec files

--- src/app/percentage.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PercetangePipe } from './percetange.pipe';
-
-describe('PercetangePipe', () => {
-  it('create an instance', () => {
-    const pipe = new PercetangePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
