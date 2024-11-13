96a3e10170105756207a179e924a9526321856ea
bcharier <baptiste.charier2022@campus-eni.fr>
2024-11-13 12:02:56 +0100

    add toaster

--- src/app/history/history.page.ts
+++ src/app/history/history.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LoveService } from '../love.service';
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-history',
@@ -8,17 +9,38 @@ import { LoveService } from '../love.service';
 })
 export class HistoryPage implements OnInit {
 
-  constructor(readonly service: LoveService) { }
+  constructor(readonly service: LoveService, readonly alertController: AlertController, readonly toastController: ToastController) { }
 
   get history() {
     return this.service.history;
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-  clearHistory() {
-    this.service.clear();
+async clearHistory() {
+    const alert = await this.alertController.create({
+      header: 'Clear history',
+      message: 'Are you sure you want to clear the history?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => this.alertController.dismiss(),
+        },
+        {
+          text: 'Confirm',
+          role: 'confirm',
+          handler: async () => {
+            this.service.clear();
+            const toast = await this.toastController.create({
+              message: 'History cleared',
+              duration: 2000,
+            });
+            await toast.present();
+          }
+        }
+      ]
+    });
+    await alert.present();
   }
-
 }
